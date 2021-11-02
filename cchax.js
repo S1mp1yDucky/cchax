f8e0358e67e2f2cd68e05a02c3794f31cccbbbfb
S1mp1y Ducky <33154919+S1mp1yDucky@users.noreply.github.com>
2021-11-02 13:17:20 +0500

    Fixed input Error, Added more Features, ++update

--- cchax.js
+++ cchax.js
@@ -3,9 +3,18 @@
 //--------------------------------------------
 // Libiraries
 const axios = require('axios');
-const readline = require("readline");
+const prompt = require('prompt-sync')({sigint: true});
 var figlet = require('figlet');
 const { exit } = require('process');
+
+//await Key press thing
+const keypress = async () => {
+    process.stdin.setRawMode(true)
+    return new Promise(resolve => process.stdin.once('data', () => {
+      process.stdin.setRawMode(false)
+      resolve()
+    }))
+  }
 
 // Clear the console
 process.stdout.write('\033c')
@@ -16,12 +25,6 @@ start()
 // Start Point
 function start() {
     process.stdout.write('\033c')
-
-// User Input Prompt
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 // Boot image
 figlet.text('CC  DUCK  WORKS', {
@@ -45,23 +48,40 @@ figlet.text('CC  DUCK  WORKS', {
     console.log(" ")
     console.log(" ------------------------ Options -------------------------- ")
     console.log(" ")
+    console.log(" 0) Guide me Sesi Ducky :)")
+    console.log(" ")
     console.log(" 1) Generate Credit Card")
     console.log(" 2) Verify Credit Card")
+    console.log(" 3) BIN Number Checker")
+    console.log(" 4) Generate my FAKE ID (Ready to card)")
     console.log(" ")
     menu()
 })
 
 // Making user make the preferred choice and executing
 function menu() {
-    rl.question("What option would you like to choose:  ", function(option) {
-        if (option == 1) {
+    const option = prompt("Choose an option: ");
+    if (option == 1) {
             ccgenerator()
             console.log(" ")
-        } else if (option ==2) {
+        }
+    else if (option == 2) {
             ccverify()
             console.log(" ")
         }
-        else {
+    else if (option == 0) {
+            uraskid()
+            console.log(" ")
+        }
+    else if (option == 4){
+            fakeid()
+            console.log(" ")
+        }
+    else if (option == 3) {
+        binchk()
+        console.log(" ")
+    }
+    else {
             process.stdout.write('\033c')
             console.log(" ")
             console.log(" ")
@@ -79,10 +99,55 @@ function menu() {
                 start()
             }, 5000);
         }
-    });
 }
 
 // functions
+//------------
+// The guide for the skids you know
+async function uraskid() {
+    process.stdout.write('\033c')
+        figlet.text('SKIDS AREA', {
+            font: 'doom',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 120,
+            whitespaceBreak: true
+        }, function(err, data) {
+            if (err) {
+                console.log('Error | Something went wrong...')
+                console.dir(err);
+                return;
+            }
+            console.log(data);
+            console.log("                                                                By S1mp1y Ducky")
+        })
+        console.log(" -----------------------------  BIN METHOD (MOST EFFECTIVE) -------------------------------------")
+        console.log(" ")
+        console.log(" 1) Find a bin from a bank from USA or your country")
+        console.log(" 2) Use our Bin Code checker to verify details and confirm your bin code")
+        console.log(" 3) Generate a credit card using your 6 digit Bin code")
+        console.log(" 4) Copy the generated credit card")
+        console.log(" 5) Use our Credit card checker to verify the card one last time")
+        console.log(" 6) Finally, Use our Fake ID generator to complete so you can fill in the details")
+        console.log(" ")
+        console.log(" ------------------------------------- HAPPY CARDING ----------------------------------------------")
+        console.log(" ")
+        console.log(" ")
+        console.log(" --------------------------- GENERATOR METHOD (LESS EFFECTIVE) ---------------------------------")
+        console.log(" ")
+        console.log(" 1) Select the generate a credit card option")
+        console.log(" 2) Select the type of credit card you want to generate")
+        console.log(" 3) Copy the generated credit card")
+        console.log(" 4) Use our Credit card checker to verify the card one last time")
+        console.log(" 5) Finally, Use our Fake ID generator to complete so you can fill in the details")
+        console.log(" ")
+        console.log(" ------------------------------------- HAPPY CARDING -----------------------------------------------")
+        console.log(" ")
+        console.log(" Press Any Key To Continue")
+        await keypress()
+        start()
+
+}
 //------------
 // Credit Card Generator
 async function ccgenerator() {
@@ -95,7 +160,7 @@ async function ccgenerator() {
             whitespaceBreak: true
         }, function(err, data) {
             if (err) {
-                console.log('Error | Something went wrong...');
+                console.log('Error | Something went wrong...')
                 console.dir(err);
                 return;
             }
@@ -112,7 +177,7 @@ async function ccgenerator() {
         console.log(" 7) [BIN] - Generate Number from 6 digits BIN number")
         console.log(" 8) ----   GO BACK TO MENU   ----")
         console.log(" ")
-        rl.question("What Credit Card would you like to Generate:  ", function(option2) {
+        const option2 = prompt("What Credit Card would you like to Generate:  ");
         if (option2 == 1) {
             visagen()
         } else if (option2 == 2) {
@@ -147,10 +212,9 @@ async function ccgenerator() {
                 start()
             }, 5000);
         }
-    });
 }
 
-function visagen() {
+async function visagen() {
     process.stdout.write('\033c')
         figlet.text('VISA GENERATOR', {
             font: 'doom',
@@ -175,13 +239,13 @@ function visagen() {
           console.log("Card Type: "+ response.data.card);
           console.log("Card Number: "+ response.data.number);
           console.log(" ")
-          setTimeout(() => {
-            return (start());
-        }, 10000);
+          console.log(" Press Any Key To Continue")
         })
+        await keypress()
+        ccgenerator()
 }
 
-function mastercardgen() {
+async function mastercardgen() {
     process.stdout.write('\033c')
         figlet.text('VISA GENERATOR', {
             font: 'doom',
@@ -206,13 +270,13 @@ function mastercardgen() {
           console.log("Card Type: "+ response.data.card);
           console.log("Card Number: "+ response.data.number);
           console.log(" ")
-          setTimeout(() => {
-            return (start());
-        }, 10000);
+          console.log(" Press Any Key To Continue")
         })
+        await keypress()
+        ccgenerator()
 }
 
-function amexgen() {
+async function amexgen() {
     process.stdout.write('\033c')
         figlet.text('AMEX GENERATOR', {
             font: 'doom',
@@ -237,13 +301,13 @@ function amexgen() {
           console.log("Card Type: "+ response.data.card);
           console.log("Card Number: "+ response.data.number);
           console.log(" ")
-          setTimeout(() => {
-            return (start());
-        }, 10000);
+          console.log(" Press Any Key To Continue")
         })
+        await keypress()
+        ccgenerator()
 }
 
-function jcbgen() {
+async function jcbgen() {
     process.stdout.write('\033c')
         figlet.text('JCB GENERATOR', {
             font: 'doom',
@@ -268,13 +332,13 @@ function jcbgen() {
           console.log("Card Type: "+ response.data.card);
           console.log("Card Number: "+ response.data.number);
           console.log(" ")
-          setTimeout(() => {
-            return (start());
-        }, 10000);
+          console.log(" Press Any Key To Continue")
         })
+        await keypress()
+        ccgenerator()
 }
 
-function dinersgen() {
+async function dinersgen() {
     process.stdout.write('\033c')
         figlet.text('DINERS GENERATOR', {
             font: 'doom',
@@ -299,13 +363,13 @@ function dinersgen() {
           console.log("Card Type: "+ response.data.card);
           console.log("Card Number: "+ response.data.number);
           console.log(" ")
-          setTimeout(() => {
-            return (start());
-        }, 10000);
+          console.log(" Press Any Key To Continue")
         })
+        await keypress()
+        ccgenerator()
 }
 
-function bingen() {
+async function bingen() {
     process.stdout.write('\033c')
         figlet.text('BIN CC GENERATOR', {
             font: 'doom',
@@ -322,7 +386,14 @@ function bingen() {
             console.log(data);
             console.log("                                                                By S1mp1y Ducky")
         })
-        rl.question('Enter a Credit Card BIN: ', (bin) => {
+        console.log(" ")
+        console.log(" [INFO] | If you want to return to the menu, Type 0")
+        console.log(" ")
+        const bin = prompt('Enter a Credit Card BIN: ');
+        if (bin == 0) {
+            ccgenerator()
+            console.log()
+        } else {
         axios.get(`https://api.bincodes.com/cc-gen/?format=json&api_key=e36641d95618424d962ba7c2a23ab6c9&input=${bin}`)
         .then(response => {
           console.log(" ")
@@ -331,11 +402,11 @@ function bingen() {
           console.log("Card Type: "+ response.data.card);
           console.log("Card Number: "+ response.data.number);
           console.log(" ")
-          setTimeout(() => {
-            return (start());
-        }, 10000);
+          console.log(" Press Any Key To Continue")
         })
-    });
+    await keypress()
+    ccgenerator()
+    }
 }
 
 
@@ -356,7 +427,14 @@ async function ccverify() {
         console.log(data);
         console.log("                                                                By S1mp1y Ducky")
     })
-    rl.question('Enter a Credit Card Number: ', (ccnum) => {
+    console.log(" ")
+    console.log(" [INFO] | If you want to return to the menu, Type 0")
+    console.log(" ")
+    const ccnum = prompt('Enter a Credit Card Number: ');
+    if (ccnum == 0){
+        start()
+        console.log()
+    } else {
     axios.get(`https://api.bincodes.com/cc/?format=json&api_key=e36641d95618424d962ba7c2a23ab6c9&cc=${ccnum}`)
     .then(response => {
       console.log(" ")
@@ -373,11 +451,76 @@ async function ccverify() {
       console.log("Issuer Phone Number: "+ response.data.phone);
       console.log("Is Card LIVE: "+ response.data.valid);
       console.log(" ")
-      setTimeout(() => {
-        return (start());
-    }, 10000);
+     console.log(" Press Any Key To Continue")
     })
-})
+await keypress()
+start()
 }
-
+}
+async function binchk() {
+    process.stdout.write('\033c')
+    figlet.text('BIN CHECKER', {
+        font: 'doom',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 120,
+        whitespaceBreak: true
+    }, function(err, data) {
+        if (err) {
+            console.log('Error | Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+        console.log("                                                                By S1mp1y Ducky")
+    })
+    console.log(" ")
+    console.log(" [INFO] | If you want to return to the menu, Type 0")
+    console.log(" ")
+    const binnum = prompt('Enter a BIN Number: ');
+    if (binnum == 0){
+        start()
+        console.log()
+    } else {
+    axios.get(`https://api.bincodes.com/bin/?format=json&api_key=e36641d95618424d962ba7c2a23ab6c9&bin=${binnum}`)
+    .then(response => {
+      console.log(" ")
+      console.log("Your BIN Number has been Checked!")
+      console.log(" ")
+      console.log("BIN Number: "+ response.data.bin);
+      console.log("Bank Name: "+ response.data.bank);
+      console.log("Card Detector: "+ response.data.card);
+      console.log("Card Type: "+ response.data.type);
+      console.log("Card Level: "+ response.data.level);
+      console.log("Card Country: "+ response.data.country);
+      console.log("Card Country Code: "+ response.data.countrycode);
+      console.log("Issuer Website: "+ response.data.website);
+      console.log("Issuer Phone Number: "+ response.data.phone);
+      console.log("Is Card LIVE: "+ response.data.valid);
+      console.log(" ")
+     console.log(" Press Any Key To Continue")
+    })
+await keypress()
+start()
+}
+}
+async function fakeid() {
+    process.stdout.write('\033c')
+    figlet.text('FAKE  ID', {
+        font: 'doom',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 120,
+        whitespaceBreak: true
+    }, function(err, data) {
+        if (err) {
+            console.log('Error | Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+        console.log("                                                                By S1mp1y Ducky")
+    })
+    console.log("        COMING SOOON          ")
+}
 }
